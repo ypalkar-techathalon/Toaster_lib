0f156aa89b12fe322a5215bb040d2db53335dc52
Yash_Palkar <ypalkar@techathalon.com>
2022-05-24 18:19:39 +0530

    update

--- testlibrary/src/main/java/com/techathalon/testlibrary/ToasterMessage.java
+++ testlibrary/src/main/java/com/techathalon/testlibrary/ToasterMessage.java
@@ -4,7 +4,7 @@ import android.content.Context;
 import android.widget.Toast;
 
 public class ToasterMessage {
-    public static void s(Context c, String message){
+    public static void sa(Context c, String message){
 
         Toast.makeText(c,message,Toast.LENGTH_SHORT).show();
 
