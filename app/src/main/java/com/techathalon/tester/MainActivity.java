9041e8354a33aa41d8d72b85e7941d5baea87027
Yash_Palkar <ypalkar@techathalon.com>
2022-05-24 17:21:43 +0530

    Initial Commit

---
+++ app/src/main/java/com/techathalon/tester/MainActivity.java
@@ -0,0 +1,14 @@
+package com.techathalon.tester;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
